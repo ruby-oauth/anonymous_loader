eb9b54b0b8a62d8982ed17605a22ed259ac48b24
Peter H. Boling <peter.boling@gmail.com>
2026-06-18 07:20:36 -0600

    Init

---
+++ lib/anonymous_loader.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anonymous_loader/version"
+
+module AnonymousLoader
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/anonymous_loader/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AnonymousLoader
+  VERSION = "0.1.0"
+end
