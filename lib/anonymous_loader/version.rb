79f8fdbb77a8c10fb9dd9d328d9c14a28e691c60
Peter H. Boling <peter.boling@gmail.com>
2026-07-31 23:06:16 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/anonymous_loader/version.rb
+++ lib/anonymous_loader/version.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module AnonymousLoader
+  # Version namespace for this gem.
   module Version
+    # Current gem version.
     VERSION = "0.1.4"
   end
+  # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
 end
